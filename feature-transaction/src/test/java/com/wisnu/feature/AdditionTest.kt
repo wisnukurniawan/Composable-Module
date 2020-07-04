ff55b47df8e430b04e6e42bbb53c3bd9449225f7
wisnu kurniawan <wisnuko13@gmail.com>
2020-07-04 20:46:22 +0700

    Add unit test for test

---
+++ feature-transaction/src/test/java/com/wisnu/feature/AdditionTest.kt
@@ -0,0 +1,13 @@
+package com.wisnu.feature
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class AdditionTest {
+
+    @Test
+    fun add() {
+        assertEquals(2, 1 + 1)
+    }
+
+}
